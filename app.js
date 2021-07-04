46d8ec8fb7d3cee9bcd15d1d016171f79269e5be
Divya <dkdivya252002@gmail.com>
2021-07-04 14:39:48 +0530

    commit

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ let app = express();
 var PORT=process.env.PORT || 7000;
 let server = require( 'http' ).Server( app );
 let io = require( 'socket.io' )( server );
-let stream = require( './ws/stream' );
+let stream = require( './src/ws/stream' );
 let path = require( 'path' );
 let favicon = require( 'serve-favicon' );
 
-app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+app.use( favicon( path.join( __dirname, 'src/favicon.ico' ) ) );
+app.use( '/assets', express.static( path.join( __dirname, 'src/assets' ) ) );
 
 app.get( '/', ( req, res ) => {
-    res.sendFile( __dirname + '/index.html' );
+    res.sendFile( __dirname + '/src/index.html' );
 } );
 
 
